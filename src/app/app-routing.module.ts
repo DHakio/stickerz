92d5985d0b0afe04d4116c8bdf3ca4fb0969bf47
DHakio <D.Hakio@gmail.com>
2020-12-13 10:00:53 +0100

    Index + Login view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { IndexComponent } from './index/index.component';
+import { CardComponent } from './products/card/card.component';
+import { OneProductComponent } from './products/one-product/one-product.component';
+import { ProductsComponent } from './products/products.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RewardsComponent } from './rewards/rewards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'rewards', component: RewardsComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'one-product', component: OneProductComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'product-card', component: CardComponent},
+  {path: '', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { RewardsComponent } from './rewards/rewards.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { ProfileComponent } from './profile/profile.component';
+import { IndexComponent } from './index/index.component';
+import { ProductsComponent } from './products/products.component';
+import { CardComponent } from './products/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent,
+    RewardsComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    ProfileComponent,
+    IndexComponent,
+    ProductsComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
